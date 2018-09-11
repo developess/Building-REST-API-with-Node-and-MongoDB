dc8c81427a64bcbd5d23cce173205f0f9517f89f
Jessie Beech <jessie@jessiebeech.com>
2018-09-11 21:33:42 +0100

    update to index.js to run db and middleware

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
-import express from 'express';
-import constants from './config/constants'
+import express from "express";
+import constants from "./config/constants";
+import "./config/database";
+import middlewareConfig from "./config/middleware";
 
 const app = express();
+middlewareConfig(app);
 
 app.listen(constants.PORT, err => {
-  if (err)
-    throw err;
+  if (err) throw err;
   else {
-    console.log(`Server running on port: ${constants.PORT} \n---\n Running on ${process.env.NODE_ENV} \n---\n Make something great!`)
-  }  
-
+    console.log(
+      `Server running on port: ${constants.PORT} \n---\n Running on ${
+        process.env.NODE_ENV
+      } \n---\n Make something great!`
+    );
+  }
 });
